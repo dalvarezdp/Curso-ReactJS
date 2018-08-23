11ba722a6e85f3d6a611c0379a50587169d07dfd
David Álvarez <david11988@gmail.com>
2018-08-23 18:45:00 +0200

    Trabajando con listas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ConditionalSection from "./sections/conditional";
+import cars from "./data/cars.json";
 
 // function Hello(props) {
 //   return <h1 className="App-title">{props.title}</h1>;
@@ -84,8 +86,27 @@ class ContadorNumero extends Component {
   }
 }
 
+class CarItem extends Component {
+  render() {
+    const { car, id } = this.props;
+    return (
+      <li>
+        <p>Key: {id}</p>
+        <p>
+          <strong>Nombre:</strong> {car.name}
+        </p>
+        <p>
+          <strong>Marca:</strong> {car.company}
+        </p>
+      </li>
+    );
+  }
+}
+
 class App extends Component {
   render() {
+    const numbers = [1, 1, 3, 4, 5];
+
     return (
       <div className="App">
         <header className="App-header">
@@ -103,6 +124,17 @@ class App extends Component {
         />
         <Title />
         <Contador contadorInicial={10} />
+        <ConditionalSection />
+        <h4>Trabajando con listas</h4>
+        {numbers.map((number, index) => {
+          return <p key={index}>Soy el número {number}</p>;
+        })}
+        <h4>Trabajando con listas con objetos</h4>
+        <ul>
+          {cars.map(car => {
+            return <CarItem id={car.id} key={car.id} car={car} />;
+          })}
+        </ul>
       </div>
     );
   }
